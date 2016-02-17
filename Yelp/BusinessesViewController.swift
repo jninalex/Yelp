52fa6bacf8f6bfa71f947d18a029e072e1e9fec3
Jeanne Nina Alexandra L. Veneracion <jninalex@yahoo.com>
2016-02-17 18:38:29 -0500

    Initial Commit

---
+++ Yelp/BusinessesViewController.swift
@@ -0,0 +1,127 @@
+//
+//  BusinessesViewController.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 4/23/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+
+    var businesses: [Business]!
+    var businessesBackup: [Business]!
+    var searchBar: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        searchBar.delegate = self
+        
+        // progamatically add search bar
+        searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+        
+        
+        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData()
+        
+            for business in businesses {
+                print(business.name!)
+                print(business.address!)
+            }
+        })
+
+/* Example of Yelp search with more search options specified
+        Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+        
+            for business in businesses {
+                print(business.name!)
+                print(business.address!)
+            }
+        }
+*/
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil {
+            return businesses!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
+        
+        cell.business = businesses[indexPath.row]
+        
+        return cell
+    }
+    
+    // Searchbar
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if(businessesBackup == nil) {
+            businessesBackup = businesses
+        }
+        
+        if searchText.isEmpty {
+            businesses = businessesBackup
+        } else {
+            // The user has entered text into the search box
+            // Use the filter method to iterate over all items in the data array
+            // For each item, return true if the item should be included and false if the
+            // item should NOT be included
+            businesses = businesses.filter({(dataItem: Business) -> Bool in
+                
+                // If dataItem matches the searchText, return true to include it
+                if dataItem.name!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil {
+                    return true
+                } else {
+                    return false
+                }
+            })
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
